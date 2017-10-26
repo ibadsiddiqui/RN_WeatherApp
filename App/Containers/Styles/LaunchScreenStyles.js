b0e8af8ac6446f59e4d6128d9c96a3a306fb2f23
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-10-26 20:40:09 +0500

    styles updated

--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -6,13 +6,42 @@ export default StyleSheet.create({
   container: {
     paddingBottom: Metrics.baseMargin
   },
-  logo: {
-    marginTop: Metrics.doubleSection,
-    height: Metrics.images.logo,
-    width: Metrics.images.logo,
-    resizeMode: 'contain'
+  header: {
+    marginTop: Metrics.doubleSection - 40,
+    marginLeft: Metrics.screenWidth - 230,
+    color: '#000',
+    fontSize: 28,
+    fontWeight: '700',
+    fontFamily: "cursive",
   },
-  centered: {
-    alignItems: 'center'
+
+  contentContainer: {
+    paddingVertical: 10,
+    paddingBottom:20
+  },
+
+  AddBtnRow:{
+    flexDirection: 'row',
+    marginTop: 10,
+    paddingLeft: Metrics.section * 5 + 5,
+    marginTop: Metrics.screenHeight - (Metrics.screenWidth/4.8),
+    position: 'absolute',
+    width: Metrics.screenWidth,
+    backgroundColor: 'white',
+    paddingBottom: 40
+    // marginLeft: Metrics.screenHeight - (Metrics.screenWidth + Metrics.section)
+  },
+  btnText:{
+    marginTop: 13,
+    fontSize: 14,
+    color: '#000',
+    marginRight: 5
+  },
+  iconStyle:{
+    backgroundColor: "#1565c0",  
+    marginTop: 10,    
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 100
   }
 })
