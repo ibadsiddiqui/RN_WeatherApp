ae9718a9d525b3ad260b5d00623b91e6c59d4ae1
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-10-26 20:40:15 +0500

    navigation added

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,12 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import TestingScreen  from "../Containers/TestingScreen";
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+    LaunchScreen: { screen: LaunchScreen },
+    newScreen : { screen: TestingScreen}
 }, {
   // Default config for all screens
   headerMode: 'none',
