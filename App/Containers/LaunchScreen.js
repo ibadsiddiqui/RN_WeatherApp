040e8c3129b27d7340a160fe98b250363ad38929
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-10-26 20:40:00 +0500

    launchscreen updated

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,28 +1,120 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
+import { 
+  Button, 
+  Text, 
+  Image, 
+  TouchableOpacity,
+  View,
+  StatusBar,
+  ScrollView
+} from 'react-native'
+
+import {Icon,List, ListItem} from 'react-native-elements'
+import {navigate} from 'react-navigation'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
+
 export default class LaunchScreen extends Component {
+  
   render () {
+    const list = [
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      //... // more items
+    ]
+    
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
+        <StatusBar backgroundColor="transparent" transulcent={true}/>
+          <Text style={styles.header}>WindLove</Text>
+          
+          <ScrollView
+            automaticallyAdjustContentInsets 
+            contentContainerStyle={styles.contentContainer}>
+            <List containerStyle={{marginBottom: 20}}>
+              {
+                list.map((l, i) => (
+                  <ListItem
+                    roundAvatar
+                    avatar={{uri:l.avatar_url}}
+                    key={i}
+                    title={l.name}
+                  />
+                ))
+              }
+            </List>    
+          </ScrollView>
 
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
+          <TouchableOpacity 
+            onPress={() => this.props.navigation.navigate('newScreen')} 
+            style={styles.AddBtnRow}>
+          
+            <Text style={styles.btnText}>Add Station </Text>
+            <Icon name='add'  color='#fff' style={styles.iconStyle}/>
+        
+          </TouchableOpacity>
+        
 
-        </ScrollView>
       </View>
     )
   }
