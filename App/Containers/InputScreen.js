5ceac71c9794d7d9eed1bb1093e0c5232bcc3eec
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-10-29 03:31:14 +0500

    UI for input was updated

--- App/Containers/InputScreen.js
+++ App/Containers/InputScreen.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react'
 import { 
+    Image,
     TextInput, 
     Text, 
-    Image, 
-    View 
+    TouchableHighlight, 
+    View,
+    ScrollView
 } from 'react-native'
+import { Button } from 'react-native-elements'
 
+//navigation
 import {navigate} from 'react-navigation'
+
+//images
+import { Images } from '../Themes/'
+
 // Styles
-import styles from './Styles/LaunchScreenStyles'
+import styles from './Styles/InputScreenStyles'
 
 export default class InputScreen extends Component {
   render () {
     return (
         <View style={styles.mainContainer}>
-            <View style={styles.inputStyles}>
-                <TextInput maxlength={1} />
-                <TextInput maxlength={1} />    
-                <TextInput maxlength={1} />
-                <TextInput maxlength={1} />
-                <TextInput maxlength={1} />
-                <TextInput maxlength={1} />
-            </View>
+            <TouchableHighlight onPress={() => this.props.navigation.navigate('LaunchScreen')}>
+                <Image source={Images.backButton}/>
+            </TouchableHighlight>
+
+            <ScrollView>
+                <View style={styles.centered}>
+                    <Text> Please Enter the Station ID</Text>
+                    <View style={styles.rowView}>
+                        <TextInput maxLength={1} />
+                        <TextInput maxLength={1} />    
+                        <TextInput maxLength={1} />
+                        <TextInput maxLength={1} />
+                        <TextInput maxLength={1} />
+                        <TextInput maxLength={1} />
+                    </View>
+
+                    <Button title='Add Station'
+                            buttonStyle={{"margin": 10}} 
+                            backgroundColor="#f44336"
+                            color="white" 
+                            borderRadius={100}/>
+                </View>
+            </ScrollView>
+                    
         </View>
     )
   }
