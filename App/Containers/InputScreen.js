a0c819c0857ee8951ebf95c41825a2cda96df7f8
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-10-29 21:29:35 +0500

    UI updated

--- App/Containers/InputScreen.js
+++ App/Containers/InputScreen.js
@@ -18,35 +18,52 @@ import { Images } from '../Themes/'
 // Styles
 import styles from './Styles/InputScreenStyles'
 
+// firebase
+import firebase from 'firebase'
+
 export default class InputScreen extends Component {
-  render () {
-    return (
-        <View style={styles.mainContainer}>
-            <TouchableHighlight onPress={() => this.props.navigation.navigate('LaunchScreen')}>
-                <Image source={Images.backButton}/>
-            </TouchableHighlight>
 
-            <ScrollView>
-                <View style={styles.centered}>
-                    <Text> Please Enter the Station ID</Text>
-                    <View style={styles.rowView}>
-                        <TextInput maxLength={1} />
-                        <TextInput maxLength={1} />    
-                        <TextInput maxLength={1} />
-                        <TextInput maxLength={1} />
-                        <TextInput maxLength={1} />
-                        <TextInput maxLength={1} />
+    render () {
+        let database = firebase.database();
+        return (
+            <View style={styles.mainContainer}>
+                <TouchableHighlight onPress={() => this.props.navigation.navigate('LaunchScreen')}>
+                    <Image source={Images.backButton}/>
+                </TouchableHighlight>
+
+                <ScrollView>
+                    <View style={styles.centered}>
+                        <Text> Please Enter the Station ID</Text>
+                        <View style={styles.rowView}>
+                            <TextInput maxLength={1} 
+                                onChangeText={(text)=> this.setState({stationID:text})}/>
+                            <TextInput maxLength={1} 
+                                onChangeText={  (text)=> 
+                                                this.setState(prevState => {stationID: prevState + text})}/>
+                            <TextInput maxLength={1} 
+                                onChangeText={  (text)=> 
+                                                this.setState(prevState => {stationId: prevState + text})}/>
+                            <TextInput maxLength={1} 
+                                onChangeText={  (text)=> 
+                                                this.setState(prevState => {stationId: prevState + text})}/>
+                            <TextInput maxLength={1} 
+                                onChangeText={  (text)=> 
+                                                this.setState(prevState => {stationId: prevState + text})}/>
+                            <TextInput maxLength={1} 
+                                onChangeText={  (text)=> 
+                                                this.setState(prevState => {stationId: prevState + text})}/>
+                        </View>
+
+                        <Button color="white"
+                                backgroundColor="#f44336"
+                                borderRadius={100}
+                                buttonStyle={{margin: 10}}
+                                onPress={()=>this.onSubmit.bind(this)} 
+                                title='Add Station'/>
                     </View>
-
-                    <Button title='Add Station'
-                            buttonStyle={{"margin": 10}} 
-                            backgroundColor="#f44336"
-                            color="white" 
-                            borderRadius={100}/>
-                </View>
-            </ScrollView>
-                    
-        </View>
-    )
-  }
+                </ScrollView>
+                        
+            </View>
+        )
+    }
 }
