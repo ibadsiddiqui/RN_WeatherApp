a32b8cb62968a6d90af8e5d86c066d535bc0213b
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-11-01 22:56:18 +0500

    placeholders and API fetching to Server added

--- App/Containers/InputScreen.js
+++ App/Containers/InputScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { 
     Image,
     TextInput, 
+    ToastAndroid,
     Text, 
     TouchableHighlight, 
     View,
@@ -28,6 +29,52 @@ export default class InputScreen extends Component {
             stationID:'',
         }
     }
+    onSubmit(){
+        const {stationID} = this.state;
+
+        let api = {
+          getWeather(){
+            var url =   `https://api.mesowest.net/v2/stations/nearesttime?stid=`
+                        + stationID.toString().toLowerCase() +
+                        `&within=10&token=4d80764baf2343b097648565e5372a7f`;
+            return  fetch(url).then((res)=> res.json())
+          }
+        }
+        
+        let date_value;
+        api.getWeather().then( 
+            res =>  
+                date_value = (res.STATION[0].OBSERVATIONS.air_temp_value_1.date_time)
+        )
+        let date_array = date_value.split('T')
+        let date_date = date_array[0]
+        let date_time = date_array[1].replace('Z','')
+        console.log(date_date, date_time)
+        
+        
+
+        api.getWeather()
+        .then( res => 
+            this.setState({
+                StationName: res.STATION[0].NAME,
+                ReadingDate: date_date, 
+                ReadingTime: date_time, 
+                Temp : res.STATION[0].OBSERVATIONS.air_temp_value_1.value * (9/5) + 32,
+                // Gust: res.STATION[0].OBSERVATIONS.wind_gust_value_1.value,
+                windDirection: res.STATION[0].OBSERVATIONS.wind_cardinal_direction_value_1d.value,
+                WindSpeed: res.STATION[0].OBSERVATIONS.wind_speed_value_1.value * 2.23693629 ,
+            }))
+        firebase.database().ref('stations/' + this.state.StationID).set({
+                StationID: this.state.stationID,
+                StationName: this.state.StationName,
+                Temp : this.state.Temp,
+                // Gust: gust,
+                ReadingTime: date_time,
+                ReadingDate: date_date,
+                windDirection: this.state.windDirection,
+                WindSpeed: this.state.WindSpeed,
+        });
+    }
     render () {
         let database = firebase.database();
         return (
@@ -40,21 +87,39 @@ export default class InputScreen extends Component {
                     <View style={styles.centered}>
                         <Text> Please Enter the Station ID</Text>
                         <View style={styles.rowView}>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={(text)=> this.setState({stationID:text})}/>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={  (text)=> 
                                                 this.setState(prevState => {stationID: prevState + text})}/>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={  (text)=> 
                                                 this.setState(prevState => {stationId: prevState + text})}/>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={  (text)=> 
                                                 this.setState(prevState => {stationId: prevState + text})}/>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={  (text)=> 
                                                 this.setState(prevState => {stationId: prevState + text})}/>
-                            <TextInput maxLength={1} 
+                            <TextInput 
+                                maxLength={1}
+                                placeholder='X'
+                                style={{ textAlign: 'center'}}
                                 onChangeText={  (text)=> 
                                                 this.setState(prevState => {stationId: prevState + text})}/>
                         </View>
